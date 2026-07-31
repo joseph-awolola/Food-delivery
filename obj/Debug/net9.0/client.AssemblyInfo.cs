4c3d10c77b3385940e38aedae9e4fe9baaf68c8d
Joseph Awolola <josephawolola16@gmail.com>
2026-07-31 20:42:26 +0100

    added page

--- obj/Debug/net9.0/client.AssemblyInfo.cs
+++ obj/Debug/net9.0/client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed3485f90af29c7ea5fd44f90f1b78ec1f57d05a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b3561a0977cb9f44ecbc6e1beee29c81b9c8dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
